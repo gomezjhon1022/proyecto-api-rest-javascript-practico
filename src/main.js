da1a048c3dd1915f91db7bfd35403f700beeff31
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-07-08 21:46:26 -0500

    Lista de categorias

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ async function getTrendingMoviesPreview () {
     const res = await fetch ('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
     const movies = data.results;
-    
+
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
         const movieContainer = document.createElement('div');
@@ -19,4 +19,29 @@ async function getTrendingMoviesPreview () {
     });
 }
 
+async function getCategoriesPreview () {
+    const res = await fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+    const categories = data.genres;
+
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+
+
+    });
+}
+
 getTrendingMoviesPreview();
+
+getCategoriesPreview ();
