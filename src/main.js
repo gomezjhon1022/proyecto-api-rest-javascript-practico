08ca2ad76c801456fed7f3b55995a20dd34f9e80
Jhon Gomez <jhonjairogomez92@gmail.com>
2022-07-09 19:26:08 -0500

    Filtrando películs por categoria

--- src/main.js
+++ src/main.js
@@ -15,9 +15,12 @@ async function getTrendingMoviesPreview () {
     const { data } = await api('trending/movie/day');
     const movies = data.results;
 
+    // const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+    trendingMoviesPreviewList.innerHTML = "";
 
     movies.forEach(movie => {
-        const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
@@ -39,14 +42,21 @@ async function getCategoriesPreview () {
     const { data } = await api('genre/movie/list');
     const categories =data.genres;
 
+    // const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+    categoriesPreviewList.innerHTML = "";
+
     categories.forEach(category => {
-        const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
 
         const categoryTitle = document.createElement('h3');
         categoryTitle.classList.add('category-title');
         categoryTitle.setAttribute('id', 'id' + category.id);
+        categoryTitle.addEventListener('click', () => {
+            location.hash = `#category=${category.id}-${category.name}`;
+        });
         const categoryTitleText = document.createTextNode(category.name);
 
         categoryTitle.appendChild(categoryTitleText);
@@ -57,7 +67,32 @@ async function getCategoriesPreview () {
     });
 }
 
+async function getMoviesByCategory(id) {
 
+    const { data } = await api('discover/movie', {
+        params: {
+            with_genres: id,
+        }
+    });
+    const movies = data.results;
+
+    genericSection.innerHTML = "";
+
+    movies.forEach(movie => {
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        genericSection.appendChild(movieContainer);
+
+    });
+}
 
 
 
